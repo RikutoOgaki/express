dbde80b42ca622bb65c5a4801e3da8db9dee4b07
Rikuto <orikutobasuke@gmail.com>
2024-10-01 10:58:01 +0900

    タスク追加機能の追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,39 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm } from 'react-hook-form'
 
 type TodoTypes = {
   id: number,
   todo: string
 }
 
+type AddTodoType = {
+  todo: string
+}
+
 export default function Home() {
 
-  const [todos, setTodos] = useState<TodoTypes[]>([])
+  const { register, handleSubmit } = useForm<AddTodoType>();
+  const [todos, setTodos] = useState<TodoTypes[]>([]);
+
+  const addTodo = async (event: AddTodoType) => {
+    const { todo } = event;
+    console.log(todo);
+    await axios
+      .post('http://localhost:3000/add', {
+        data: {
+          todo,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        const todo = response.data;
+        setTodos((preTodos) => [todo, ...preTodos]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     axios
@@ -22,6 +47,10 @@ export default function Home() {
 
   return (
     <>
+      <form onSubmit={handleSubmit(addTodo)}>
+        <input {...register('todo')} type="text" />
+        <button type="submit">add</button>
+      </form>
       <div>
         {todos.map((v) =>
           <p key={v.id}>{v.todo}</p>
